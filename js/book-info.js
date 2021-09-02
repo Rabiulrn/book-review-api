805e7195f7e96c683eb0db27f99fff69166884b9
Rabiulrn <rnrabiul@gmail.com>
2021-09-02 23:38:34 +0600

    almost

--- js/book-info.js
+++ js/book-info.js
@@ -1,43 +1,94 @@
- document.getElementById('error-message').style.display = 'none';
+
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     // clear data
-    searchField.value = '';                                              //ager showing result remove kore notun result dekhabe
-    
+    searchField.value = '';
+    // numFound Count
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => totalResult(data))                                              
+    const totalResult = data =>{
+        const foundCount = document.getElementById('num-found');
+        foundCount.innerHTML = `total result found All:${data.numFound} `;
+    }
+    //empty search
     if (searchText == '') {
-        // please write something to display
+        console.log('please inseart something');
     }
     else {
         // load data
-        const url = `http://openlibrary.org/search.json?q=${searchText}`;
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
         fetch(url)
             .then(res => res.json())
-            .then(data => displaySearchResult(data.docs[0]))
-            .catch(error => displayError(error));                        //error catch
+            .then(data => displaySearchResult(data.docs))                  
+                              
     }
 }
-const displayError = error => {
-    document.getElementById('error-message').style.display = 'block';            //error khaile error msg show.
-}
+
 const displaySearchResult = books => {
     console.log(books)
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
-    // if (books.length == 0) {
-    //     // show no result found;
-    // }
+    if (books.length == 0) {
+        console.log('no result found');
+        const errMsg = document.getElementById('error-message');
+        errMsg.innerHTML = `no result found`;
+    }
+    // errMsg.textContent = '';
+    let c = 0;
     books.forEach(book => {
-         console.log(book.title);
+        // console.log(book.author_name[0]);
+        // const index = indexOf('book');
+        c = c + 1;
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
             <div class="card-body">
+                ${loadCover(book.cover_i)}
                 <h5 class="card-title">${book.title}</h5>
-                 <p class="card-text">${book.title}</p>
+                <p class="card-text">${book.author_name?.[0]}
+                <p class="card-text">${book.publisher?.[0]}
+                 <p class="card-text">${book.first_publish_year}</p>
             </div>
         
         `;
         searchResult.appendChild(div);
     })
+    const foundCount = document.getElementById('found-count');
+    foundCount.innerHTML = `total result found :${c} `;
+    
 }
+const loadCover = coverId => {
+    const url = `https://covers.openlibrary.org/b/id/${coverId}-M.jpg`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCoverImage(data));
+}
+const displayCoverImage = coverIds =>{
+
+    console.log(coverIds);
+    const searchResult = document.getElementById('search-result');
+    const div = document.createElement('div');
+    div.innerHTML =`<img src="${coverIds.cover_i}-M.jpg" class="card-img-top" alt="...">`
+    searchResult.appendChild(div);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// {/* <p class="card-text">index:${c}</p> */}
+//<p class="card-text">${book.author_name[0]}</p>
+//<p class="card-text">${book.publisher[0]}</p>
+// <p class="card-text">${book.author_name?.[0]}</p>
